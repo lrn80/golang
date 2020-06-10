f5517ae668e58f0204272129ba70967bca719d17
lrn80 <1349539656@qq.com>
2020-06-10 21:44:55 +0800

    feat: 常量的定义

--- hello.go
+++ hello.go
@@ -11,7 +11,37 @@ func varZeroValue()  {
 	fmt.Println(a, b, c, d, e)
 }
 
+func consts()  {
+	const a, b = 3, 4
+	const (
+		php = 1
+		python = 2
+		js = "javasrcipt"
+	)
+
+	println(php, python, js, a, b)
+}
+
+func enums()  {
+	const (
+		cpp = iota
+		java
+		ruby
+	)
+
+	const(
+		b = 1 << (10 * iota)
+		kb
+		mb
+		gb
+	)
+	println(cpp, java, ruby)
+	println(b, kb, mb, gb)
+}
+
 func main()  {
 	fmt.Println("Hello World")
 	varZeroValue()
+	consts()
+	enums()
 }
