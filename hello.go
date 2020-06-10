4e32457a70b62e03ec04dd69f17462224d557a90
lrn80 <1349539656@qq.com>
2020-06-10 20:47:16 +0800

    feat: 变量的定义

--- hello.go
+++ hello.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func varZeroValue()  {
+	var a int      // 使用 var 定义
+	var str string
+	var  d , e float32
+	b, c := 3, true // 使用 : 定义
+	fmt.Printf(str)
+	fmt.Println(a, b, c, d, e)
+}
+
 func main()  {
 	fmt.Println("Hello World")
+	varZeroValue()
 }
