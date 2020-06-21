e6a1f7c7f3f00c0a4401874548b885ae76620cc2
lrn80 <1349539656@qq.com>
2020-06-21 22:05:24 +0800

    fix: 最长不含重复字符的字串

--- exampleMap.go
+++ exampleMap.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+func lengthOfLongestSubstring(s string) int {
+	l, max := 0, 0
+	m := make(map[byte]int)
+	for r, ch := range []byte(s) {
+		lastR, ok := m[ch]
+		if ok && lastR >= l {
+			l = lastR + 1
+		}
+		if r-l+1 > max {
+			max =  r - l + 1
+		}
+		m[ch] = r
+	}
+
+	return max;
+}
+
 func main() {
-	$END$
+	re := lengthOfLongestSubstring("aaaaccc")
+	fmt.Println(re);
 }
