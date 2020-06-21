5e7ed632f71a5e767c97cfd5bbdf1c1dec8372af
lrn80 <1349539656@qq.com>
2020-06-21 22:04:24 +0800

    fix: 字符串的操作

--- string.go
+++ string.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	$END$
+	str := "I love you!"
+
+	bytes := []byte(str) // 转换成字节
+
+	fmt.Println(bytes)  // [73 32 108 111 118 101 32 121 111 117 33]
+
 }
