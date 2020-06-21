d6f20b9ebdaa585ff2fc7ff38b09a928eee3f2d4
lrn80 <1349539656@qq.com>
2020-06-21 22:01:35 +0800

    fix: 数组中切片的操作

--- slice.go
+++ slice.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	s := arr[2:5] // [2,5)
+	fmt.Println(s) // [2 3 4]
+
+	s = arr[:9] // 从 a[0] 到 a[9],但是不包括 a[9]
+	fmt.Println(s)
+
+	s = arr[2:] // 从a[2] 到 a[9]，且包括 a[2]
+	fmt.Println(s)
 }
