388fab9502beb1c791afeb454af0c2943c05bf9b
lrn80 <1349539656@qq.com>
2020-06-21 21:10:26 +0800

    fix: array数组的定义和遍历

--- array.go
+++ array.go
@@ -4,17 +4,30 @@ import "fmt"
 
 func main() {
 	var arr [3]int
-	 arr1 := [3]int{2, 3,4}
-	 arr2 := [...]int{5, 6, 7, 8}
-	var grid [4][5]int
-	 fmt.Println(arr, arr1, arr2, grid)
+	fmt.Println(arr) // [0 0 0] 初始值为 0
 
-	 // 数组的遍历
+	arr1 := [3]int{2, 3,4}
+	 fmt.Println(arr1) // [2 3 4]
+
+	arr2 := [...]int{5, 6, 7, 8}
+	 fmt.Println(arr2) // [5 6 7 8] 未给出固定的初始长度
+
+	var grid [4][5]int
+	 fmt.Println(grid) // [[0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0]] 二维数组 4行 5列
+
+	// 数组的遍历
 	for i := 0; i < len(arr2); i++ {
-		fmt.Println(arr2[i]);
+		fmt.Println(arr2[i]); // 5
+							  // 6
+							  // 7
+							  // 8
 	}
+
 	// 数组的遍历
 	for i, v := range arr2{
-		fmt.Println(i, v);
+		fmt.Println(i, v); // 0 5
+		                   // 1 6
+		                   // 2 7
+		                   // 3 8
 	}
 }
